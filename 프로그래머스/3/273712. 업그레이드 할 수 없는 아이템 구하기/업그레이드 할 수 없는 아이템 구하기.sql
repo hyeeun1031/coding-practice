656537b772e90ea081ce29c126ac1ae9f6269163
Hyeeun <1031hyeeun@naver.com>
2025-08-20 00:16:01 +0900

    [level 3] Title: 업그레이드 할 수 없는 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/273712. 업그레이드 할 수 없는 아이템 구하기/업그레이드 할 수 없는 아이템 구하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 작성해주세요
+SELECT ii.ITEM_ID, ii.ITEM_NAME, ii.RARITY
+FROM ITEM_INFO AS ii
+WHERE ii.ITEM_ID NOT IN (SELECT DISTINCT PARENT_ITEM_ID
+                         FROM ITEM_TREE
+                         WHERE PARENT_ITEM_ID IS NOT NULL)
+ORDER BY ii.ITEM_ID DESC;
