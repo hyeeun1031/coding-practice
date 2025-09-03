004f9a24103a373692d163ed92ccbd23b42b9cb7
Hyeeun <1031hyeeun@naver.com>
2025-09-04 01:53:17 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 107.62 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
@@ -1,3 +1,10 @@
 def solution(arr, k):
     # k가 홀수일 경우 각 원소에 k를 곱함, 짝수일 경우 각 원소에 k를 더함
-    return [x * k if k % 2 == 1 else x + k for x in arr]
+    
+    result = []
+    for x in arr:
+        if k % 2 == 1:      # k가 홀수일 때
+            result.append(x * k)
+        else:               # k가 짝수일 때
+            result.append(x + k)
+    return result
