88c2daf10ebc336a18f201f1ec8e4c57b4c17cd0
Hyeeun <1031hyeeun@naver.com>
2025-09-03 16:59:48 +0900

    Create solution.sql

---
+++ 해커랭크/SQL/Basic/Weather Observation Station 8/solution.sql
@@ -0,0 +1,13 @@
+# Language: MySQL
+SELECT DISTINCT CITY
+FROM STATION
+WHERE (CITY LIKE 'a%' OR
+       CITY LIKE 'e%' OR
+       CITY LIKE 'i%' OR
+       CITY LIKE 'o%' OR
+       CITY LIKE 'u%') AND
+      (CITY LIKE '%a' OR
+       CITY LIKE '%e' OR
+       CITY LIKE '%i' OR
+       CITY LIKE '%o' OR
+       CITY LIKE '%u');
