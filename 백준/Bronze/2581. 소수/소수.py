c026893c93a7f748cbc099f02325e9beb5f334bd
Hyeeun <1031hyeeun@naver.com>
2025-09-04 01:37:09 +0900

    [Bronze II] Title: 소수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+M = int(input().strip())
+N = int(input().strip())
+
+def is_prime(x):
+    if x<2:
+        return False
+    for i in range(2, int(x**0.5)+1):
+        if x%i == 0:
+            return False
+    return True
+
+primes = []
+for i in range(M, N+1):
+    if is_prime(i):
+        primes.append(i)
+
+if primes:
+    print(sum(primes))
+    print(min(primes))
+else:
+    print(-1)
