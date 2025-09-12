97e9737d12b81f7156db9eaff81d185f62ca48f9
Hyeeun <1031hyeeun@naver.com>
2025-09-13 00:37:21 +0800

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,17 @@
+def solution(num):
+    if num == 1:
+        return 0
+    
+    count = 0
+    
+    while num != 1 and count < 500:
+        if num % 2 == 0:
+            num = num // 2
+        else:
+            num = num*3 + 1
+        count += 1
+        
+    if num == 1:
+        return count
+    else:
+        return -1
