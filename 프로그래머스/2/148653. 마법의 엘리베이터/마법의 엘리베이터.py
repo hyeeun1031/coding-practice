3fcef9bae13d404c360e55aaa96190b6fd5ee2c7
Hyeeun <1031hyeeun@naver.com>
2026-09-05 00:45:48 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -0,0 +1,20 @@
+def solution(storey):
+    answer = 0
+
+    while storey > 0:
+        digit = storey % 10
+        storey //= 10
+
+        if digit < 5:
+            answer += digit
+        elif digit > 5:
+            answer += 10 - digit
+            storey += 1
+        else:  # digit == 5
+            if storey % 10 >= 5:
+                answer += 5
+                storey += 1
+            else:
+                answer += 5
+
+    return answer
